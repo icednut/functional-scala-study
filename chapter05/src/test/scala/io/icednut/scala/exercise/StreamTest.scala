eca12fc09ae695e48b477dbc85e8109f5daa0a9e
Will <will.109@kakaopaycorp.com>
2019-11-04 07:10:08 +0900

    chapter 5 연습문제 추가

---
+++ chapter05/src/test/scala/io/icednut/scala/exercise/StreamTest.scala
@@ -0,0 +1,22 @@
+package io.icednut.scala.exercise
+
+import org.scalatest.FunSuite
+
+/**
+ * @author will.109
+ * @date 04/11/2019
+ **/
+class StreamTest extends FunSuite {
+
+  test("[연습문제 5.1] toList를 구현해야 한다.") {
+    pending
+  }
+
+  test("[연습문제 5.2] drop(n)을 구현해야 한다.") {
+    pending
+  }
+
+  test("[연습문제 5.3] takeWhile을 구현해야 한다.") {
+    pending
+  }
+}
